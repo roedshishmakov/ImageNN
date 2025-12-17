acdfead0236decc88e77611d99bb930df06e2254
roedshishmakov <dnsmarketingofficial@gmail.com>
2025-12-17 14:18:21 +0300

    Added programm to train default model

---
+++ default_train.py
@@ -0,0 +1,133 @@
+import tools
+import activations
+import keras
+
+from main import *
+from tools import *
+from PIL import Image
+
+def edit_x_data(traindata, p):
+    """
+    Функция подготовки входа датасета
+
+    :param traindata: датасет
+    :type traindata: list
+    :param p: количество примеров
+    :type p: int
+    :return: список входных значений
+    :rtype: list
+    """
+
+    resarr = []
+    size = 16
+    i = 0
+    for x in traindata[:p]:
+        arr = flat(Image.fromarray(x).resize((size, size)))
+        rr = []
+        for z in arr:
+            if z > 150:
+                rr.append(1)
+            else: rr.append(0)
+        resarr.append(rr)
+        i += 1
+    return resarr
+
+def edit_y_data(traindata, p):
+    """
+    Функция подготовки выходов для обучения
+
+    :param traindata: датасет
+    :type traindata: list
+    :param p: число ответов
+    :type p: int
+    :return: список ответов
+    :rtype: list
+    """
+
+    resarr = []
+    for x in traindata[:p]:
+        a = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+        a[x] = 1
+        resarr.append(a)
+    return resarr
+
+def load_mnist():
+    """
+    Функция загрузки датасета mnist
+
+    :return: список входов и выходов
+    :rtype: list
+    """
+
+    train_data = []
+    test_data = []
+
+    train_num = 1000
+    test_num = 100
+
+    (x_train, y_train), (x_test, y_test) = keras.datasets.mnist.load_data()
+    x_train_data = edit_x_data(x_train, train_num)
+    x_test_data = edit_x_data(x_test, test_num)
+
+    y_train_data = edit_y_data(y_train, train_num)
+    y_test_data = edit_y_data(y_test, test_num)
+
+    for i in range(len(x_train_data)):
+        train_data.append((x_train_data[i], y_train_data[i]))
+    for i in range(len(x_test_data)):
+        test_data.append((x_test_data[i], y_test_data[i]))
+    return [train_data, test_data]
+
+DATA_FILE_NAME = "weight_saves/mnist1.json"
+LOSS_FILE_NAME = 'loss_saves/mnist1.txt'
+
+nn = NeuralNetwork()
+nn.add_input_layer(256)
+nn.add_layer(32, activation_class = activations.ActivationRelu, random_radius=0.5, use_bias = True)
+nn.add_layer(32, activation_class = activations.ActivationSigmoid, random_radius=0.5, use_bias = True)
+nn.add_layer(10, activation_class = activations.ActivationSigmoid, random_radius=0.5, use_bias = True)
+
+mnist = load_mnist()
+train_data, test_data = mnist[0], mnist[1]
+print("MNIST loaded!")
+
+print("Do you want to load saved model?(y/n):", end = ' ')
+z = input()
+if z == 'y':
+    print("Enter name of save:", end = ' ')
+    DATA_FILE_NAME = "weight_saves/" + input() + ".json"
+    import_data = tools.import_json(DATA_FILE_NAME)
+    nn.import_(import_data)
+else:
+    print("Enter new save name:", end = ' ')
+    z = input()
+    DATA_FILE_NAME = "weight_saves/" + z + ".json"
+    LOSS_FILE_NAME = "loss_saves/" + z + ".txt"
+    total_loss_statistics = []
+    print("Enter number of Epochs:", end = ' ')
+    ep = int(input())
+    for i in range(ep):
+        print('EPOCH #{}'.format(i))
+        loss_total = nn.train(train_data, 0.1)
+        print('LOSS: {:.4f}'.format(loss_total))
+        total_loss_statistics.append(loss_total)
+
+    tools.export_json(DATA_FILE_NAME, nn.export())
+    f = open(LOSS_FILE_NAME, 'w')
+    for s in total_loss_statistics:
+        f.write(str(s) + '\n')
+    f.close()
+
+print("Show loss graph?(y/n):", end = ' ')
+ans = input()
+if ans == 'y':
+    show_loss_save(LOSS_FILE_NAME)
+else: print()
+
+print("Enter number of tests:", end = ' ')
+test_len = int(input())
+for i in range(test_len):
+    nn.run(test_data[i][0])
+    print("Answer:", nn.get_best_index())
+    print("Correct answer:", test_data[i][1].index(1))
+    print()
